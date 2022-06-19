5f2c9e963ac6c65c2afc4ceeec657b37b9a9806b
Felix Hahn <info@hahn-felix.de>
2022-06-19 21:33:35 +0200

    Add Topic List Widget for User Profile

---
+++ widgets/TopicListUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace humhub\modules\themeOrange\widgets;
+
+use humhub\modules\content\components\ContentContainerController;
+use humhub\modules\topic\models\Topic;
+use humhub\modules\content\helpers\ContentContainerHelper;
+use humhub\modules\user\models\User;
+use humhub\modules\ui\menu\MenuLink;
+use humhub\modules\ui\menu\widgets\LeftNavigation;
+use Yii;
+use yii\base\Exception;
+
+class TopicListUser extends LeftNavigation
+{
+
+    /** @var User */
+    public $user;
+
+    /** @var Space */
+    public $id = 'user-topic-menu';
+
+    public $contentContainer;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if (!$this->user) {
+            $this->user = ContentContainerHelper::getCurrent(User::class);
+        }
+        
+        $this->panelTitle = "<b>" . Yii::t('TopicModule.base', 'Topics') . "</b>";
+        $topics = Topic::findByContainer($this->user)->all();
+
+        foreach ($topics as $topic) {
+            /* @var $topic Topic */
+            $this->addEntry(new MenuLink([
+                'label' => $topic->name,
+                'url' => $topic->getUrl(),
+                'icon' => 'fa-tag',
+                'sortOrder' => $topic->sort_order,
+            ]));
+        }
+
+        parent::init();
+    }
+}

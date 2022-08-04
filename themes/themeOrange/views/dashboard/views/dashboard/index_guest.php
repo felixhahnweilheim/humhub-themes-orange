8830e0fb9bc597061b723a016e3d057761b9c9ca
Felix Hahn <info@hahn-felix.de>
2022-08-04 16:09:56 +0200

    Add Language Chooser to guest dashboard

--- themes/themeOrange/views/dashboard/views/dashboard/index_guest.php
+++ themes/themeOrange/views/dashboard/views/dashboard/index_guest.php
@@ -4,13 +4,19 @@ use humhub\libs\Html;
 use humhub\modules\dashboard\widgets\DashboardContent;
 use humhub\modules\dashboard\widgets\Sidebar;
 use humhub\widgets\FooterMenu;
+use humhub\widgets\LanguageChooser;
 
 ?>
 
 <?= Html::beginContainer(); ?>
 <div class="row">
-    <div class="col-md-12 layout-content-container">
+    <div class="col-md-6">
+        <?= LanguageChooser::widget(); ?>
+    </div>
+    <div class="col-md-6">
         <?= FooterMenu::widget(['location' => FooterMenu::LOCATION_SIDEBAR]); ?>
+    </div>
+    <div class="col-md-12 layout-content-container">
         <?= DashboardContent::widget(); ?>
     </div>
 </div>

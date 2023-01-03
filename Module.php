197ff580a0f511b39d35e3fc941b66dae8a91522
Felix Hahn <info@hahn-felix.de>
2023-01-03 14:30:59 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -2,10 +2,25 @@
 
 namespace humhub\modules\themeOrange;
 
+use humhub\modules\ui\view\helpers\ThemeHelper;
+
 class Module extends \humhub\components\Module
 {
-	    /**
-	     *      * @inheritdoc
-	     *           */
-	    public $resourcesPath = 'resources';    
+    /**
+    * @inheritdoc
+    */
+    public $resourcesPath = 'resources';
+    
+	public $theme;
+	
+	public function disable() {
+    
+	    // Deselect theme (select community theme)
+		$theme = ThemeHelper::getThemeByName('HumHub');
+		if ($theme !== null) {
+			$theme->activate();
+		}
+	
+        parent::disable();
+    }
 }

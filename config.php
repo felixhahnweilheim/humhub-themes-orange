cccefab771c56d78330006ab362edfa3e495458a
Felix Hahn <info@hahn-felix.de>
2023-01-03 14:18:15 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,12 @@
 return [
     'id' => 'theme-orange',
     'class' => 'humhub\modules\themeOrange\Module',
-    'namespace' => 'humhub\modules\themeOrange'
+    'namespace' => 'humhub\modules\themeOrange',
+    'events' => [
+		[
+			'class' => ModuleManager::class,
+			'event' => ModuleManager::EVENT_AFTER_MODULE_ENABLE,
+			'callback' => [Events::class, 'onModuleEnabled']
+		]
+	]
 ];

16d416f7b3fe59a52c55197d4a8c8fa37f042e31
Felix Hahn <info@hahn-felix.de>
2023-01-04 15:35:55 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -10,8 +10,17 @@ class Module extends \humhub\components\Module
     * @inheritdoc
     */
     public $resourcesPath = 'resources';
-    
+	
+    /*Setting for comment link (options: icon, text, both)*/
+	public $commentLink = 'icon';
+	
 	public $theme;
+	
+	public static function getCommentLinkSetting() {
+		
+		$module = Yii::$app->getModule('theme-orange');
+        return $module ? $module->commentLink : 'icon';
+	}
 	
 	public function disable() {
     

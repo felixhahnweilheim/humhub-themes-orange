1074afaa75148568cb326d3e8e3a30bb2d594670
Felix Hahn <info@hahn-felix.de>
2023-02-17 21:33:33 +0100

    Update Events.php

--- Events.php
+++ Events.php
@@ -6,6 +6,8 @@ use Yii;
 use yii\base\Model;
 use humhub\modules\ui\view\helpers\ThemeHelper;
 use yii\base\Theme;
+use yii\helpers\Url;
+
 
 class Events
 {
@@ -20,5 +22,26 @@ class Events
         if ($theme !== null) {
             $theme->activate();
         }
+		
+		/*Add Module settings*/
+		$module = Yii::$app->getModule('theme-orange');
+		$module->settings->set('commentLink', $module->commentLink);
+	    $module->settings->set('likeLink', $module->likeLink);
+		$module->settings->set('likeIcon', $module->likeIcon);
     }
+    
+	public static function onAdminMenuInit($event)
+    {
+		$event->sender->addItem([
+            'label' =>  Yii::t('ThemeOrangeModule.base', 'Theme Orange Configuration'),
+            'url' => Url::to(['/theme-orange/config']),
+            'group' => 'manage',
+            'icon' => '<i class="fa fa-eye"></i>',
+            'isActive' => (Yii::$app->controller->module
+                    && Yii::$app->controller->module->id === 'theme-orange'
+                    && (Yii::$app->controller->id === 'page' || Yii::$app->controller->id === 'config')),
+            'sortOrder' => 900,
+            'isVisible' => true,
+        ]);
+	}
 }

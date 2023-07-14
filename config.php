abdb3dc544b2d786c866dd6ebe80292645f6e705
Felix Hahn <info@hahn-felix.de>
2023-07-14 13:27:04 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,8 @@
 <?php
 
-use yii\base\Event;
-use humhub\components\ModuleManager;
-use humhub\modules\admin\widgets\AdminMenu;
-use humhub\modules\themeOrange\Events;
-
 return [
     'id' => 'theme-orange',
     'class' => 'humhub\modules\themeOrange\Module',
     'namespace' => 'humhub\modules\themeOrange',
-    'events' => [
-	[
-	    'class' => AdminMenu::class,
-	    'event' => AdminMenu::EVENT_INIT,
-	    'callback' => [Events::class, 'onAdminMenuInit']
-	],
-    ],
+    'events' => [],
 ];
